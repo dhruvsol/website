828bcbc6a267f90d23c5605f75ac425bf5d42d75
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-04-21 12:12:54 +0530

    feat: added a video

---
+++ components/Social.jsx
@@ -0,0 +1,19 @@
+
+export const Social = () => {
+  return (
+    <>
+        <div className="bg-black">
+        <div className="">
+            <div className="flex justify-center">
+                <h1 className="text-white text-2xl md:text-3xl lg:text-4xl xl:text-5xl hover:text-purple hover:animate-pulse">
+                    Connect With Me
+                </h1>
+            </div>
+        </div>
+            <video autoPlay loop muted style={{ width: "100%" }}>
+            <source src="/so1.webm" />
+            </video>
+        </div>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { HeroSection } from "../components/HeroSection";
 import Navbar from "../components/Navbar";
+import { Social } from "../components/Social";
+
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Social />
     </>
   );
 }
