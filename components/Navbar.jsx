7431230b0d7f497d3895cbeea232a3ab929073cc
0xdhruv.sol <89624156+dhruvsol@users.noreply.github.com>
2022-07-16 23:19:25 +0530

    testing

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <div className="hidden md:flex ">
           <ul className="flex justify-around gap-5 items-center">
             <li className="text-white hover:text-green text-md font-bold">
-              <Link href="/">About me</Link>
+              <Link href="solana:https%3A%2F%2F581f-183-83-166-234.ngrok.io%2Fapi%2Fv1%2Funique%2Fmint%3Fid%3DAVLSazf3-M7uwasoXhEqj?label=Made+with+%3C3+by+CandyPay&message=Thanks+for+minting+NFTs%21">About me</Link>
             </li>
             <li className="text-white hover:text-green text-md font-bold">
               <Link href="/">Projects</Link>
